ae7ebc7e844127eb2ce8717c40c229c889bef7a8
Vitor Diniz <dinizadroit@gmail.com>
2023-03-03 11:22:05 -0300

    Projeto estruturado

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/defaultTheme";
+
+export function App() {
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Header />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  )
+}
+
+
