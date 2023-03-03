a4f8652785d773f77b3823c625670e70fe71f306
Vitor Diniz <dinizadroit@gmail.com>
2023-03-03 15:34:47 -0300

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,45 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+
 import * as S from './styles'
+import banner from '../../assets/coffee-delivery-banner.svg'
 
 export function Home() {
-  return <S.HomeContainer>Home</S.HomeContainer>
+  return (
+    <S.HomeContainer>
+      <S.LeftContainer>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+        <S.StampsContainer>
+          <S.StampItem variant="yellow-dark" customSize={true}>
+            <div>
+              <ShoppingCart size={16} />
+            </div>
+            <span>Compra simples e segura</span>
+          </S.StampItem>
+          <S.StampItem variant="gray">
+            <div>
+              <Package size={16} />
+            </div>
+            <span>Embalagem mantém o café intacto</span>
+          </S.StampItem>
+          <S.StampItem variant="yellow" customSize={true}>
+            <div>
+              <Timer size={16} />
+            </div>
+            <span>Entrega rápida e rastreada</span>
+          </S.StampItem>
+          <S.StampItem variant="purple">
+            <div>
+              <Coffee size={16} />
+            </div>
+            <span>O café chega fresquinho até você</span>
+          </S.StampItem>
+        </S.StampsContainer>
+      </S.LeftContainer>
+      <img src={banner} alt="" />
+    </S.HomeContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,74 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { pxToRem } from '../../utils/pxToRem'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  display: flex;
+  gap: ${pxToRem(56)};
+`
+
+export const LeftContainer = styled.div`
+  margin-top: ${pxToRem(94)};
+
+  h1 {
+    font-size: ${pxToRem(48)};
+    font-weight: 800;
+    line-height: 1.3;
+    color: ${(props) => props.theme['base-title']};
+  }
+
+  p {
+    margin-top: ${pxToRem(16)};
+    font-size: ${pxToRem(20)};
+    line-height: 1.3;
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+`
+
+export const StampsContainer = styled.div`
+  margin-top: ${pxToRem(66)};
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`
+
+const VARIANT_COLORS = {
+  gray: 'base-text',
+  purple: 'purple',
+  'yellow-dark': 'yellow-dark',
+  yellow: 'yellow',
+} as const
+
+interface StampItemProps {
+  variant: keyof typeof VARIANT_COLORS
+  customSize?: boolean
+}
+
+export const StampItem = styled.div<StampItemProps>`
+  display: flex;
+  align-items: center;
+  gap: ${pxToRem(12)};
+
+  ${(props) =>
+    props.customSize &&
+    css`
+      width: ${pxToRem(251)};
+    `};
+
+  div {
+    padding: ${pxToRem(8)};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${(props) => props.theme[VARIANT_COLORS[props.variant]]};
+    border-radius: 50%;
+
+    svg {
+      color: ${(props) => props.theme.background};
+    }
+  }
+
+  span {
+    line-height: 1.3;
+    color: ${(props) => props.theme['base-text']};
+  }
+`
