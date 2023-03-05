5470a91f3cc9e8187670da565e9a74f46ebf4010
Vitor Diniz <dinizadroit@gmail.com>
2023-03-05 10:45:53 -0300

    coffees e cart quantity

--- src/contexts/CoffeesContext.tsx
+++ src/contexts/CoffeesContext.tsx
@@ -1,9 +1,36 @@
-import { createContext, PropsWithChildren } from 'react'
+import { createContext, PropsWithChildren, useState } from 'react'
 
-export const CoffeesContext = createContext({} as any)
+interface CoffeesProps {
+  id: number
+  name: string
+  description: string
+  price: number
+  tags: string[]
+}
+
+interface CoffeesContextProps {
+  coffees: CoffeesProps[]
+  cartQuantity: number
+}
+
+export const CoffeesContext = createContext({} as CoffeesContextProps)
 
 export function CoffeesContextProvider({ children }: PropsWithChildren) {
+  const [coffees, setCoffees] = useState<CoffeesProps[]>([
+    {
+      id: 1,
+      name: 'Cubano',
+      description:
+        'Drink gelado de café expresso com rum, creme de leite e hortelã',
+      price: 9.9,
+      tags: ['Especial', 'Alcoólico', 'Gelado'],
+    },
+  ])
+  const [cartQuantity, setCartQuantity] = useState<number>(0)
+
   return (
-    <CoffeesContext.Provider value={{}}>{children}</CoffeesContext.Provider>
+    <CoffeesContext.Provider value={{ cartQuantity, coffees }}>
+      {children}
+    </CoffeesContext.Provider>
   )
 }
