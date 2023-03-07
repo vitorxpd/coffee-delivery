cfeadcbf1396e27af1be20e726de3bf5fd3ebfb1
Vitor Diniz <dinizadroit@gmail.com>
2023-03-07 11:07:04 -0300

    componente Payment Methods

--- src/pages/Checkout/components/PaymentMethods/index.tsx
+++ src/pages/Checkout/components/PaymentMethods/index.tsx
@@ -1,5 +1,64 @@
+import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
+
 import * as S from './styles'
 
 export function PaymentMethods() {
-  return <S.PaymentContainer>Formas de Pagamento</S.PaymentContainer>
+  function handleSelectMethod(event: any) {
+    document
+      .querySelectorAll('input[type="radio"]')
+      .forEach((element) => element.classList.remove('checked'))
+
+    event.target.classList.add('checked')
+  }
+
+  return (
+    <S.PaymentMethodsContainer>
+      <S.PaymentDescriptionContainer>
+        <CurrencyDollar size={22} />
+        <S.PaymentDescriptionTextContainer>
+          <S.DescriptionTitle>Endereço de Entrega</S.DescriptionTitle>
+          <S.DescriptionSubtitle>
+            Informe o endereço onde deseja receber seu pedido
+          </S.DescriptionSubtitle>
+        </S.PaymentDescriptionTextContainer>
+      </S.PaymentDescriptionContainer>
+      <S.MethodsContainer>
+        <input
+          type="radio"
+          id="credito"
+          name="payment_method"
+          onClick={handleSelectMethod}
+          hidden
+        />
+        <S.RadioLabel htmlFor="credito">
+          <CreditCard size={16} />
+          <S.TextLabel>Cartão de Crédito</S.TextLabel>
+        </S.RadioLabel>
+
+        <input
+          type="radio"
+          id="debito"
+          name="payment_method"
+          onClick={handleSelectMethod}
+          hidden
+        />
+        <S.RadioLabel htmlFor="debito">
+          <Bank size={16} />
+          <S.TextLabel>Cartão de Débito</S.TextLabel>
+        </S.RadioLabel>
+
+        <input
+          type="radio"
+          id="dinheiro"
+          name="payment_method"
+          onClick={handleSelectMethod}
+          hidden
+        />
+        <S.RadioLabel htmlFor="dinheiro">
+          <Money size={16} />
+          <S.TextLabel>Dinheiro</S.TextLabel>
+        </S.RadioLabel>
+      </S.MethodsContainer>
+    </S.PaymentMethodsContainer>
+  )
 }
--- src/pages/Checkout/components/PaymentMethods/styles.ts
+++ src/pages/Checkout/components/PaymentMethods/styles.ts
@@ -1,9 +1,77 @@
 import styled from 'styled-components'
 import { pxToRem } from '../../../../utils/pxToRem'
 
-export const PaymentContainer = styled.div`
+export const PaymentMethodsContainer = styled.div`
   margin-top: ${pxToRem(12)};
   padding: ${pxToRem(40)};
   background: ${(props) => props.theme['base-card']};
   border-radius: 6px;
+`
+
+export const PaymentDescriptionContainer = styled.div`
+  display: flex;
+  gap: ${pxToRem(8)};
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+`
+
+export const PaymentDescriptionTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(2)};
+`
+
+export const DescriptionTitle = styled.span`
+  line-height: 1.3;
+  color: ${(props) => props.theme['base-subtitle']};
+`
+
+export const DescriptionSubtitle = styled.span`
+  font-size: ${pxToRem(14)};
+  line-height: 1.3;
+  color: ${(props) => props.theme['base-text']};
+`
+
+export const MethodsContainer = styled.div`
+  margin-top: ${pxToRem(32)};
+  display: flex;
+  gap: ${pxToRem(12)};
+
+  input[type='radio'].checked + label {
+    padding: ${pxToRem(15)};
+    background: ${(props) => props.theme['purple-light']};
+    border: 1px solid ${(props) => props.theme.purple};
+  }
+`
+
+export const RadioLabel = styled.label`
+  width: ${pxToRem(178)};
+  padding: ${pxToRem(16)};
+  display: flex;
+  gap: ${pxToRem(12)};
+  align-items: center;
+  background: ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => props.theme['base-hover']};
+
+    span {
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+  }
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+`
+
+export const TextLabel = styled.span`
+  font-size: ${pxToRem(12)};
+  line-height: 1.6;
+  color: ${(props) => props.theme['base-text']};
+  text-transform: uppercase;
 `
