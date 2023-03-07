b42aa90d1bd84d62cf633eebaa51c02dcb8d00a3
Vitor Diniz <dinizadroit@gmail.com>
2023-03-07 00:24:46 -0300

    criacao das paginas checkout e success

---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export function Checkout() {
+  return <S.CheckoutContainer>Página Checkout</S.CheckoutContainer>
+}
---
+++ src/pages/Success/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export function Success() {
+  return <S.SuccessContainer>Página Success</S.SuccessContainer>
+}
