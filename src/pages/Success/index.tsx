7d6c4bda7bffa36325b02a1ea803cca3395e23e1
Vitor Diniz <dinizadroit@gmail.com>
2023-03-14 10:37:13 -0300

    pagina success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,49 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+
 import * as S from './styles'
+import success from '../../assets/success.svg'
 
 export function Success() {
-  return <S.SuccessContainer>Página Success</S.SuccessContainer>
+  return (
+    <S.SuccessWrapper>
+      <S.MainTitle>Uhu! Pedido confirmado</S.MainTitle>
+      <S.Subtitle>
+        Agora é só aguardar que logo o café chegará até você
+      </S.Subtitle>
+      <S.InfoContent>
+        <S.BoxWithBorder>
+          <S.InfoBox>
+            <S.IconContainer variant="purple">
+              <MapPin size={16} />
+            </S.IconContainer>
+            <S.TextContent>
+              <span>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </span>
+              <span>Farrapos - Porto Alegre, RS</span>
+            </S.TextContent>
+          </S.InfoBox>
+          <S.InfoBox>
+            <S.IconContainer variant="yellow">
+              <Timer size={16} />
+            </S.IconContainer>
+            <S.TextContent>
+              <span>Previsão de entrega</span>
+              <strong>20 min - 30 min</strong>
+            </S.TextContent>
+          </S.InfoBox>
+          <S.InfoBox>
+            <S.IconContainer variant="yellow-dark">
+              <CurrencyDollar size={16} />
+            </S.IconContainer>
+            <S.TextContent>
+              <span>Pagamento na entrega</span>
+              <strong>Cartão de crédito</strong>
+            </S.TextContent>
+          </S.InfoBox>
+        </S.BoxWithBorder>
+        <img src={success} alt="" />
+      </S.InfoContent>
+    </S.SuccessWrapper>
+  )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,96 @@
 import styled from 'styled-components'
+import { pxToRem } from '../../utils/pxToRem'
 
-export const SuccessContainer = styled.main``
+export const SuccessWrapper = styled.main`
+  padding-top: ${pxToRem(80)};
+`
+
+export const MainTitle = styled.h1`
+  font-size: ${pxToRem(32)};
+  font-weight: 800;
+  line-height: 1.3;
+  color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const Subtitle = styled.p`
+  margin-top: ${pxToRem(4)};
+  font-size: ${pxToRem(20)};
+  line-height: 1.3;
+  color: ${(props) => props.theme['base-subtitle']};
+`
+
+export const InfoContent = styled.div`
+  margin-top: ${pxToRem(40)};
+  display: flex;
+  justify-content: space-between;
+`
+
+export const BoxWithBorder = styled.div`
+  width: ${pxToRem(526)};
+  padding: ${pxToRem(40)};
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(32)};
+  border-radius: 6px 36px;
+  background: ${(props) => props.theme.background};
+  position: relative;
+
+  &::before {
+    content: '';
+    background: linear-gradient(
+      102.89deg,
+      ${(props) => props.theme.yellow} 2.61%,
+      ${(props) => props.theme.purple} 98.76%
+    );
+    border-radius: 7px 37px;
+    inset: -1px;
+    position: absolute;
+    z-index: -1;
+  }
+`
+
+export const InfoBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${pxToRem(12)};
+`
+
+const VARIANT_COLORS = {
+  purple: 'purple',
+  yellow: 'yellow',
+  'yellow-dark': 'yellow-dark',
+} as const
+
+interface IconContainerProps {
+  variant: keyof typeof VARIANT_COLORS
+}
+
+export const IconContainer = styled.div<IconContainerProps>`
+  width: ${pxToRem(32)};
+  height: ${pxToRem(32)};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.theme[VARIANT_COLORS[props.variant]]};
+  border-radius: 1000px;
+
+  svg {
+    color: ${(props) => props.theme.background};
+  }
+`
+
+export const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    display: block;
+    line-height: 1.3;
+    color: ${(props) => props.theme['base-title']};
+  }
+
+  strong {
+    line-height: 1.3;
+    color: ${(props) => props.theme['base-title']};
+  }
+`
