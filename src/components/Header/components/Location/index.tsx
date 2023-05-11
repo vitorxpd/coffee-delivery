e48b496c9dd4086f16a79a3935b779d12249f4bf
Vitor Diniz <dinizadroit@gmail.com>
2023-05-11 18:09:51 -0300

    hook useLocationInfo

--- src/components/Header/components/Location/index.tsx
+++ src/components/Header/components/Location/index.tsx
@@ -1,35 +1,34 @@
 import { useEffect, useState } from 'react'
 import { MapPin } from 'phosphor-react'
-import { useMapsReverseGeocoding } from '../../../../hooks/useMapsReverseGeocoding'
 
 import * as S from './styles'
+import { useLocationInfo } from '../../../../hooks/useLocationInfo'
 
 export function Location() {
-  const [location, setLocation] = useState<string[]>([])
+  const [city, setCity] = useState<string | null>(null)
+  const [state, setState] = useState<string | null>(null)
 
-  const address = useMapsReverseGeocoding()
+  const { addressComponents } = useLocationInfo()
 
   useEffect(() => {
-    if (Object.keys(address).length) {
-      address.address_components.map((adr) => {
-        if (adr.types.includes('administrative_area_level_2')) {
-          setLocation([adr.long_name])
+    if (addressComponents && !city && !state) {
+      for (const component of addressComponents) {
+        if (component.types.includes('administrative_area_level_2')) {
+          setCity(component.long_name)
+        } else if (component.types.includes('administrative_area_level_1')) {
+          setState(component.short_name)
+        } else if (city && state) {
+          break
         }
-
-        if (adr.types.includes('administrative_area_level_1')) {
-          setLocation((state) => [...state, adr.short_name])
-        }
-
-        return adr
-      })
+      }
     }
-  }, [address])
+  }, [addressComponents, city, state])
 
   return (
     <S.LocationWrapper>
       <MapPin size={22} />
       <S.LocationText>
-        {location.length ? location.join(', ') : 'Endereço não informado.'}
+        {city && state ? `${city}, ${state}` : 'Endereço não informado'}
       </S.LocationText>
     </S.LocationWrapper>
   )
---
+++ src/hooks/useLocationInfo.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+
+interface AddressComponent {
+  long_name: string
+  short_name: string
+  types: string[]
+}
+
+interface Location {
+  addressComponents: AddressComponent[] | null
+  formattedAddress: string | null
+}
+
+export function useLocationInfo() {
+  const [location, setLocation] = useState<Location>({
+    addressComponents: null,
+    formattedAddress: null,
+  })
+
+  useEffect(() => {
+    const API_KEY = import.meta.env.VITE_MAPS_KEY
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${latitude}+${longitude}&key=${API_KEY}`
+
+      fetch(url)
+        .then(async (response) => {
+          const json = await response.json()
+          setLocation({
+            addressComponents: json.results[0].address_components,
+            formattedAddress: json.results[0].formatted_address,
+          })
+        })
+        .catch((error) => {
+          console.log(`Erro ao obter localização: ${error}`)
+        })
+    })
+  }, [])
+
+  return location
+}
