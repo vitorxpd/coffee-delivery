95bb3b51120387afedb3b2d03373b977db78d665
Vitor Diniz <dinizadroit@gmail.com>
2023-05-15 16:20:22 -0300

    fix import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import { Location } from './components/Location'
 import { Minicart } from './components/Minicart'
+import { useDeviceInfo } from '../../hooks/useDeviceInfo'
 
 import * as S from './styles'
 import logo from '../../assets/logo.svg'
-import { useDeviceInfo } from '../../hooks/useDeviceInfo'
 
 export function Header() {
   const { isMobile } = useDeviceInfo()
