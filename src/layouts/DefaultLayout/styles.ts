1b31e1c0a970918a0b6dcdf0878a700cca82c9c8
Vitor Diniz <dinizadroit@gmail.com>
2023-05-15 16:50:14 -0300

    mobile fix

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -8,6 +8,10 @@ export const LayoutWrapper = styled.div`
   padding: 0 ${pxToRem(160)};
 
   @media screen and (max-width: 1440px) {
+    padding: 0 ${pxToRem(20)};
+  }
+
+  @media screen and (max-width: 1024px) {
     padding: 0 ${pxToRem(8)};
   }
 `
