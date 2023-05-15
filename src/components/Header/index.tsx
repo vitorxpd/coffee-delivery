9b81f080a8c3ecae62cec13e30b3a9a0ed01b539
Vitor Diniz <dinizadroit@gmail.com>
2023-05-15 16:20:05 -0300

    location only desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,15 +4,18 @@ import { Minicart } from './components/Minicart'
 
 import * as S from './styles'
 import logo from '../../assets/logo.svg'
+import { useDeviceInfo } from '../../hooks/useDeviceInfo'
 
 export function Header() {
+  const { isMobile } = useDeviceInfo()
+
   return (
     <S.HeaderWrapper>
       <Link to="/">
         <img src={logo} alt="" />
       </Link>
       <S.HeaderRight>
-        <Location />
+        {!isMobile && <Location />}
         <Minicart />
       </S.HeaderRight>
     </S.HeaderWrapper>
