4506e38ca211cf36f88f089ff61252d69041251f
Vitor Diniz <dinizadroit@gmail.com>
2023-05-16 18:10:35 -0300

    refatorando useLocationInfo

--- src/hooks/useLocationInfo.ts
+++ src/hooks/useLocationInfo.ts
@@ -12,21 +12,24 @@ interface Location {
 }
 
 export function useLocationInfo(): Location {
-  const [addressComponents, setAddressComponents] = useState(null)
-  const [formattedAddress, setFormattedAddress] = useState(null)
+  const [location, setLocation] = useState<Location>({
+    addressComponents: null,
+    formattedAddress: null,
+  })
 
   useEffect(() => {
-    const API_KEY = import.meta.env.VITE_MAPS_KEY
-
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords
+      const API_KEY = import.meta.env.VITE_MAPS_KEY
       const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${latitude}+${longitude}&key=${API_KEY}`
 
       fetch(url)
         .then(async (response) => {
           const json = await response.json()
-          setAddressComponents(json.results[0].address_components)
-          setFormattedAddress(json.results[0].formatted_address)
+          setLocation({
+            addressComponents: json.results[0].address_components,
+            formattedAddress: json.results[0].formatted_address,
+          })
         })
         .catch((error) => {
           console.log(`Erro ao obter localização: ${error}`)
@@ -34,5 +37,5 @@ export function useLocationInfo(): Location {
     })
   }, [])
 
-  return { addressComponents, formattedAddress }
+  return location
 }
